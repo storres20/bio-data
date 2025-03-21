90277cc648a8a0f6b346fc811cba02791efb7271
storres20 <sanchotorres777@gmail.com>
2025-03-21 14:52:43 -0500

    new changes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -36,6 +36,7 @@ router.post('/login', async (req, res) => {
     try {
         // Find user by username
         const user = await User.findOne({ username });
+        console.log(user)
         if (!user) {
             return res.status(400).json({ message: 'Invalid username or password' });
         }
